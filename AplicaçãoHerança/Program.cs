138c2341f159dfb666421a6b75fb71384e389fdb
Samuel Braz dos Santos <braz.samuel35@uniplaclages.edu.br>
2026-04-07 22:28:43 -0300

    Criando cadastros (erro no tec adm)

--- AplicaçãoHerança/Program.cs
+++ AplicaçãoHerança/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassesHerança;
+internal class Program
+{
+
+    List<Pessoa> pessoas = new List<Pessoa>();
+
+    public Program()
+    {
+        Pessoa pessoa1 = new Pessoa("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua A", "Centro", "Cidade X"), "123", "Apto 101");
+        
+        Funcionario funcionario1 = new Funcionario("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua A", "Centro", "Cidade X"), "123", "Apto 101", "2023001", 3000);
+        
+        Aluno aluno1 = new Aluno("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua A", "Centro", "Cidade X"), "123", "Apto 101", "2023001", new Curso { Nome = "Sistemas de Informação" });
+        Aluno aluno2 = new Aluno("Maria Silva", "987.654.321-00", "MG-87.654.321", "25 anos", new Logradouro("Rua B", "Bairro Y", "Cidade Z"), "456", "Casa", "2023002", new Curso { Nome = "Engenharia de Software" });
+        Aluno aluno3 = new Aluno("João Pereira", "111.222.333-44", "MG-11.222.333", "28 anos", new Logradouro("Rua C", "Bairro W", "Cidade V"), "789", "Apto 202", "2023003", new Curso { Nome = "Ciência da Computação" });
+
+        Professor professor1 = new Professor("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua A", "Centro", "Cidade X"), "123", "Apto 101", "Mestre", "Área de Tecnologia");
+        Professor professor2 = new Professor("Luciano Coelho", "555.666.777-88", "MG-55.666.777", "40 anos", new Logradouro("Rua D", "Bairro Z", "Cidade Y"), "321", "Casa", "Doutor", "Área de Programação");
+
+        Coordenador coordenador1 = new Coordenador("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua A", "Centro", "Cidade X"), "123", "Apto 101", "Mestre", 10000, "AbcBolinhas");
+        
+        TecnicoAdministrativo tecnico1 = new TecnicoAdministrativo("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua A", "Centro", "Cidade X"), "123", "Apto 101", "2023001", 2000, "Desenvolvedor");  
+        
+        
+        }
+    private static void Main(string[] args)
+    {
+         _ = new Program();
+    }
+}
