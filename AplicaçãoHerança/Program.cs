8290d0181d4d32a7196ba66ba839ee3f33e19cb5
Samuel Braz <braz.samuel35@uniplaclages.edu.br>
2026-04-16 22:27:53 -0300

    fazendo listagem nova na main usando foreach e a lista de Pessoas RetornarDados()

--- AplicaçãoHerança/Program.cs
+++ AplicaçãoHerança/Program.cs
@@ -14,9 +14,9 @@ internal class Program
         Funcionario funcionario1 = new Funcionario("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua AbcBolinhas", "Centro", "Cidade Lages"), "123", "Apto 101", "2023001", 3000);
         listapessoas.Add(funcionario1);*/
 
-        Aluno aluno1 = new Aluno("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua AbcBolinhas", "Centro", "Cidade Lages"), "123", "Apto 101", "2023001", new Curso { Nome = "Sistemas de Informação" });
-        Aluno aluno2 = new Aluno("Maria Silva", "987.654.321-00", "MG-87.654.321", "25 anos", new Logradouro("Rua B", "Bairro Y", "Cidade Z"), "456", "Casa", "2023002", new Curso { Nome = "Engenharia de Software" });
-        Aluno aluno3 = new Aluno("João Pereira", "111.222.333-44", "MG-11.222.333", "28 anos", new Logradouro("Rua C", "Bairro W", "Cidade V"), "789", "Apto 202", "2023003", new Curso { Nome = "Ciência da Computação" });
+        Aluno aluno1 = new Aluno("Samuel Braz dos Santos", "123.456.789-00", "MG-12.345.678", "30 anos", new Logradouro("Rua AbcBolinhas", "Centro", "Cidade Lages"), "123", "Apto 101", "2023001", new Curso { Nome = "Sistemas de Informação" , Codigo = "SI2026"});
+        Aluno aluno2 = new Aluno("Maria Silva", "987.654.321-00", "MG-87.654.321", "25 anos", new Logradouro("Rua B", "Bairro Y", "Cidade Z"), "456", "Casa", "2023002", new Curso { Nome = "Engenharia de Software" , Codigo = "ES2026"});
+        Aluno aluno3 = new Aluno("João Pereira", "111.222.333-44", "MG-11.222.333", "28 anos", new Logradouro("Rua C", "Bairro W", "Cidade V"), "789", "Apto 202", "2023003", new Curso { Nome = "Ciência da Computação" , Codigo = "CC2026"});
         listapessoas.Add(aluno1);
         listapessoas.Add(aluno2);
         listapessoas.Add(aluno3);
@@ -35,10 +35,30 @@ internal class Program
         listapessoas.Add(tecnico1);
         listapessoas.Add(tecnico2);
             
+    
+        // Percorre cada pessoa cadastrada na lista
+        foreach (var item in listapessoas) 
+        {
+            // 1. Tipo do objeto (Vai imprimir "Aluno", "Professor", etc.)
+            Console.WriteLine($"Tipo do objeto: {item.GetType().Name}"); 
 
-        ListarPessoas(listapessoas);
-        
+            // 2. A representação em string do objeto (Vai usar o método ToString() de cada classe, mostrando as informações mais relevantes)
+            Console.WriteLine($"Representação textual: {item.ToString()}"); 
+            
+            // 3. O conteúdo do RetornarDados()
+            Console.WriteLine("Dados detalhados:");
+            // Como RetornarDados devolve uma lista de tuplas, fazemos outro foreach para extrair o campo e o valor
+            foreach (var (campo, valor) in item.RetornarDados()) 
+            {
+                Console.WriteLine($"  - {campo}: {valor}");
+            }
+            
+            // Uma linha em branco apenas para separar uma pessoa da outra na tela
+            Console.WriteLine("--------------------------------------------------"); 
+        }
     }
+        
+        /*listar pessoas antigo agora esta sendo usado o de cima, mas deixei aqui caso queira comparar ou usar depois:
         static void ListarPessoas(List<Pessoa> pessoas)
         {
             foreach (var pessoa in pessoas)
@@ -53,7 +73,7 @@ internal class Program
                 Console.WriteLine($"Complemento: {pessoa.Complemento}");
                 Console.WriteLine(); // linha em branco separando cada pessoa
             }
-        }
+        }*/
         private static void Main(string[] args)
         {
             _ = new Program();
